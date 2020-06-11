6c8c37a69a86c3e40d09728cb45f690c888235cf
nathan <nathanhung0918@gmail.com>
2020-06-11 15:14:05 +0800

    Initial Commit

---
+++ StudentBase/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StudentBase
+//
+//  Created by 洪語辰 on 2020/6/11.
+//  Copyright © 2020 洪語辰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
